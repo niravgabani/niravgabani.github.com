2bf5287874244dc34022a12d4b6653f6abd54e8f
nirav <niravgabani8787@gmail.com>
2025-02-11 20:03:49 +0530

    fff

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+  <title>FFF Skin Tool</title>
+
+  <!-- Inter Font -->
+  <!-- <link rel="icon" href="assets/images/icon.svg" /> -->
+
+  <!-- Inter Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+    rel="stylesheet">
+
+  <!-- Style CSS -->
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <script src="assets/js/jquery-3.6.0.min.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+</head>
+
+<body class="gradient">
+  <!-- Header -->
+  <header class="header-start">
+    <div class="wrapper">
+      <div class="py-8 grid grid-cols-2 items-center">
+        <div>
+          <a href="index.php">
+            <img class="w-48" src="assets/images/logo.svg" alt="">
+          </a>
+        </div>
+        <span class="grid justify-end">
+          <a href="index.php">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="size-6 sm:size-8 md:size-10">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+            </svg>
+          </a>
+          </snap>
+      </div>
+    </div>
+  </header>
+
+  <!-- Here is some information about the app: -->
+  <section>
+    <div class="wrapper py-10 md:py-20">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-0 items-center">
+        <div class="gap-4 lg:gap-8 grid text-center text-white lg:text-left">
+          <h2>Proved By prodesigner</h2>
+          <h2>Work that we, produce for, our clients</h2>
+          <span>
+            <a href="src/Skin/skintool.php" class="btn">Explor</a>
+          </span>
+        </div>
+        <div>
+          <img class="rounded-2xl" src="assets/images/bgimg111.png" alt="img" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Let's Start -->
+  <section>
+    <div class="wrapper py-10 md:py-24">
+      <div class="grid gap-5">
+        <a href="src/Skin/skintool.php">
+          <div
+            class="gradient1 items-center py-10 md:py-0 px-5 md:px-20 grid grid-cols-1 md:grid-cols-2 rounded-2xl shadow1">
+            <span class="grid gap-2 md:gap-5 text-center md:text-left">
+              <img class="w-8 sm:w-14 mx-auto sm:mx-0" src="assets/images/skip.webp" alt="img" />
+              <h5>Let's Start</h5>
+              <p>With FFFFF Skin Tool Diamonds, you can easily get daily free diamonds to enhance your gaming
+                experience. </p>
+            </span>
+            <img class="m-auto w-72" src="assets/images/caroline.png" alt="img" />
+          </div>
+        </a>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+          <a href="#">
+            <div
+              class="bg-purple-900 hover:bg-purple-950 text-center items-center py-10 md:py-20 grid gap-2 md:gap-4 rounded-2xl shadow1">
+              <img class="m-auto w-8 sm:w-14" src="assets/images/rate.webp" alt="img" />
+              <h4>Need More</h4>
+            </div>
+          </a>
+          <a href="src/common/privecy.php">
+            <div
+              class="bg-purple-900 hover:bg-purple-950 text-center items-center py-10 md:py-20 grid gap-2 md:gap-4 rounded-2xl shadow1">
+              <img class="m-auto w-8 sm:w-14" src="assets/images/pro.webp" alt="img" />
+              <h4>Privacy & Policy</h4>
+            </div>
+          </a>
+          <a href="src/common/privecy.php">
+            <div
+              class="bg-purple-900 hover:bg-purple-950 text-center items-center py-10 md:py-20 grid gap-2 md:gap-4 rounded-2xl shadow1">
+              <img class="m-auto w-8 sm:w-14" src="assets/images/share.webp" alt="img" />
+              <h4>Refer & Share</h4>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- footer -->
+  <footer class="border-t border-purple-900">
+    <div class="py-10 lg:py-16 wrapper">
+      <div class="items-center gap-10 sm:gap-0 grid grid-cols-1 sm:grid-cols-3 text-center sm:text-left">
+        <div>
+          <h3 class="text-white">© 2024 Copains. All Rights Reserved.</h3>
+        </div>
+        <a href="index.php" class="order-first md:order-none">
+          <img class="w-48 m-auto" src="assets/images/logo.svg" alt="img" />
+        </a>
+        <div class="flex justify-evenly sm:justify-end items-center gap-5">
+          <a href="https://www.instagram.com/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-instagram"
+              viewBox="0 0 16 16">
+              <path
+                d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script src="assets/js/jquery-3.6.0.min.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    function addClass(IdBox, ClasBox) {
+      var element = document.querySelector(IdBox);
+      element.classList.add(ClasBox);
+    }
+
+    function removeClass(IdBox, ClasBox) {
+      var element = document.querySelector(IdBox);
+      element.classList.remove(ClasBox);
+    }
+
+    // -------------------------------
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 500) {
+        $(".header-start").addClass("active");
+      } else {
+        $(".header-start").removeClass("active");
+      }
+    });
+  </script>
+</body>
+
+</html>
